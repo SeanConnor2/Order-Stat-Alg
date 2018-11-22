ccc283dc6d17e56c932d430d228625a8ce701f80
matthenschke <matthewhenschke98@gmail.com>
2018-11-22 13:45:28 -0500

    attempted to start median filtering algorithm

--- medianFiltering.cpp
+++ medianFiltering.cpp
@@ -6,9 +6,22 @@ Authors: Nora Elattar, Sean Connor, and Matthew Henschke
 */
 #include<iostream>
 #include "imageio.h"
-using namespace std;
+
 
 int main(){
-    cout << "Hello World" << endl;
+	//int * rows, * cols; // pointer arrays for rows and columns
+	//int 
+	int dim; //number of rows and colums
+	unsigned char *** image = nullptr;
+
+	std::cout << "Please enter the square dimension for your image" << std::endl;
+	std ::cin >> dim;
+	int rows = dim;
+	int col = dim;
+	//rows = new int[dim];
+	//cols = new int[dim];
+
+	//std::cout << read_pgm_image("mountain.png", image, rows, cols);
+	allocate_image(rows, col);
     return 0;
 }
