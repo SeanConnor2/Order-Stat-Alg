72a58d945bf61e0969af3ee91a182e82aaab5959
matthenschke <matthewhenschke98@gmail.com>
2018-11-28 19:02:28 -0500

    added median filtering algorithm

---
+++ median_filtering.py
@@ -0,0 +1,47 @@
+"""
+Part 2 of Project:
+Implement Median Filtering Procedure using Order Statistics Algorithm
+"""
+#libraries used
+from PIL import Image
+import numpy
+
+#median filtering algorithm
+def median_filter(data, filter_size):
+    temp = []
+    indexer = filter_size // 2
+    data_final = []
+    data_final = numpy.zeros((len(data),len(data[0])))
+    for i in range(len(data)):
+
+        for j in range(len(data[0])):
+
+            for z in range(filter_size):
+                if i + z - indexer < 0 or i + z - indexer > len(data) - 1:
+                    for c in range(filter_size):
+                        temp.append(0)
+                else:
+                    if j + z - indexer < 0 or j + indexer > len(data[0]) - 1:
+                        temp.append(0)
+                    else:
+                        for k in range(filter_size):
+                            temp.append(data[i + z - indexer][j + k - indexer])
+
+            # This is the part where we will implement the order statistics algorithm
+            temp.sort()
+            data_final[i][j] = temp[len(temp) // 2]
+            temp = []
+    return data_final
+
+
+if __name__ == "__main__":
+    img = Image.open("boat.jpg").convert("L")
+    filter = int(input("Please enter a dimension"))
+    arr = numpy.array(img)
+    removed_noise = median_filter(arr, filter)
+    img = Image.fromarray(removed_noise)
+    img.show()
+
+
+
+
